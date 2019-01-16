92616c67600a5f05921f896920255b1142f3a5f3
charisschali <teezjay11@gmail.com>
2019-01-16 14:30:42 +0300

    ft(add-user):perform crud on users -register user routes -aodify user model -add user -modify user details -users can also modify their details -admin can delete users -add tests [WIP MMDP-133]
    feat(add-user): Send Confirmation email and verify email
    
    feat(add-user): Send Confirmation email and verify email
    
    ft(add-user):perform crud on users
    
    - add update user functionality
    - add delete user functionality
    - add get user functionality
    - add get all users fuctionality
    [WIP MMDP-133]
    
    feat(add-users): perform crud operation
    
    - refactor code to conform to set standards.
    - add tests for users
    
    [WIP MMDP-133]

---
+++ server/constants/controllerConstants.js
@@ -0,0 +1,38 @@
+export const fields = {
+  email: "*",
+  id: "*",
+  username: "*",
+  isAdmin: "*",
+  confirmed: "*"
+};
+
+export const exclude = {
+  password: 0,
+  _id: 0,
+  __v: 0
+};
+
+export const passwordError = {
+  password:
+    "password must have at least 8 characters," +
+    "password must be alphanumeric," +
+    "password must have  uppercase characters," +
+    "password must have lowercase characters," +
+    "password should not contain uppercase"
+};
+
+export default {
+  accountCreated: "user successfully created",
+  verified: "Congratulations! Your account has been successfully verified.",
+  emailUpdated: "email succesfully changed",
+  notFound: "user was not found",
+  tryAgain: "something went wrong, try again",
+  userDeleted: "user was deleted successfully",
+  detailsUpdated: "You details have been successfully updated."
+};
+
+export const status = {
+  SUCCESS: "success",
+  ERROR: "error",
+  FAIL: "fail"
+};
---
+++ server/routes/api/Users.js
@@ -0,0 +1,100 @@
+import { User } from "../../models/User";
+import username from "../../utils/usernameGenerator";
+import { sendConfirmationEmail } from "../../utils/mailer";
+import { fields, exclude, status } from "../../constants/controllerConstants";
+import resp from "../../constants/controllerConstants";
+const { SUCCESS, ERROR, FAIL } = status;
+
+export const createUser = (req, res) => {
+  let user = User.model(),
+    data = req.body;
+  data["username"] = username(req.body.email);
+  user.getUpdateHandler(req).process(data, (err) => {
+    if (err) return res.json({ error: 'err' });
+    sendConfirmationEmail(data);
+    return res.status(201).json({
+      status: SUCCESS,
+      message: resp.accountCreated
+    });
+  });
+};
+
+export const confirmed = (req, res) => {
+  return res.status(200).json({
+    status: SUCCESS,
+    message: resp.verified
+  });
+};
+
+export const updateEmail = (req, res) => {
+  return res.json({
+    status: SUCCESS,
+    message: resp.emailUpdated
+  });
+};
+
+export const deleteUser = async (req, res) => {
+  await User.model.findOne({ username: req.params.id }).exec((err, user) => {
+    if (!user)
+      return res.status(404).json({
+        status: ERROR,
+        message: resp.notFound
+      });
+
+    user.remove(err => {
+      if (err)
+        return res.status(500).json({
+          status: ERROR,
+          message: resp.tryAgain
+        });
+      return res.status(200).json({
+        status: SUCCESS,
+        message: resp.userDeleted
+      });
+    });
+  });
+};
+
+export const fetchUser = async (req, res) => {
+  await User.model
+    .findOne(
+      {
+        username: req.params.id
+      },
+      fields
+    )
+    .exec((err, user) => {
+      if (!user)
+        return res.status(404).json({
+          status: FAIL,
+          message: resp.notFound
+        });
+      else {
+        return res.json({
+          status: SUCCESS,
+          user: user
+        });
+      }
+    });
+};
+
+export const fetchAllUsers = async (req, res) => {
+  const user = await User.model.find({}, exclude);
+  if (!user) {
+    return res.status(500).json({
+      status: FAIL,
+      message: resp.tryAgain
+    });
+  }
+  return res.json({
+    status: SUCCESS,
+    users: user
+  });
+};
+
+export const edited = (req, res) => {
+  return res.json({
+    status: SUCCESS,
+    message: resp.detailsUpdated
+  });
+};
