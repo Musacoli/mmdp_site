3b71fdb8af9fe10d1f32641d1d4cce63e0d098fe
Mutai Mwiti <mutaimwiti40@gmail.com>
2019-01-29 13:46:30 +0300

    feat(rbac): integrate authorization for groups resource
    - authenticate group endpoints
    - create testing helper wrapper with login() and logout()
    
    [WIP MMDP-134]

---
+++ server/__tests__/helpers/cms/about/governorMessage.js
@@ -0,0 +1,14 @@
+import {faker} from "../../base";
+import GovernorMessage from '../../../../models/GovernorMessage';
+
+export const makeGovernorMessage = (overrides = {}) => {
+  return {
+    governorName: faker.name.findName(),
+    governorMessage: faker.lorem.sentence(),
+    ...overrides
+  }
+};
+
+export const createGovernorMessage = async (overrides = {}) => {
+  return await GovernorMessage.model.create(makeGovernorMessage(overrides));
+};
