3fde7802b0457597d26e0a8e2b94a1193cfbdf96
Musacoli <collins.mbulakyalo@andela.com>
2019-02-21 02:58:37 +0300

    feat(users-mmdp-cms-search): Enable searching and filtering of users.

---
+++ client/components/common/Form/UsersSearch.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Input, Button } from 'semantic-ui-react';
+import makeAnimated from 'react-select/lib/animated';
+import Select from 'react-select';
+
+const UsersSearch = ({
+  handleChange,
+  selectedOption,
+  options,
+  handleSearch,
+  handleSearchChange,
+}) => {
+  return (
+    <div>
+      <form className="group-select">
+        <Input
+          placeholder="Search "
+          className="search-box user-search"
+          onChange={handleSearchChange}
+        />
+        <Select
+          className="userGroup"
+          label="Select Groups"
+          placeholder="All groups"
+          components={makeAnimated()}
+          onChange={handleChange}
+          value={selectedOption}
+          hideSelectedOptions={false}
+          options={options}
+          isClearable
+        />
+        <Button className="btn-search cool-blue small" onClick={handleSearch}>
+          Search
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+UsersSearch.propTypes = {
+  handleSearchChange: PropTypes.func.isRequired,
+  handleSearch: PropTypes.func.isRequired,
+  options: PropTypes.shape(),
+  selectedOption: PropTypes.shape(),
+  handleChange: PropTypes.func.isRequired,
+};
+
+export default UsersSearch;
