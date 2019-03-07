99fb0d7a2b13abe2c3f0650755591d80653afaa9
shemogumbe <shem.ogumbe@andela.com>
2019-03-07 15:26:51 +0300

    updated env variables for back end build with option envs

--- server/index.js
+++ server/index.js
@@ -4,20 +4,20 @@ import routes from './routes';
 
 dotEnv.config();
 
-const mongodbURL = () => {
-  if (process.env.NODE_ENV === 'test') {
-    const MONGODB_URL = process.env.TEST_MONGODB_URL;
-    return MONGODB_URL;
-  }
-  if (process.env.NODE_ENV === 'production') {
-    const MONGODB_URL = process.env.MONGODB_URL;
-    return MONGODB_URL;
-  }
-  if (process.env.NODE_ENV === 'development') {
-    const MONGODB_URL = process.env.DEV_MONGODB_URL;
-    return MONGODB_URL;
-  }
-};
+// const mongodbURL = () => {
+//   if (process.env.NODE_ENV === 'test') {
+//     const MONGODB_URL = process.env.TEST_MONGODB_URL;
+//     return MONGODB_URL;
+//   }
+//   if (process.env.NODE_ENV === 'production') {
+//     const MONGODB_URL = process.env.MONGODB_URL;
+//     return MONGODB_URL;
+//   }
+//   if (process.env.NODE_ENV === 'development') {
+//     const MONGODB_URL = process.env.DEV_MONGODB_URL;
+//     return MONGODB_URL;
+//   }
+// };
 
 keystone.init({
   name: 'MMDP CMS',
@@ -28,7 +28,7 @@ keystone.init({
   'auto update': true,
   session: true,
   auth: true,
-  mongo: mongodbURL(),
+  mongo: process.env.MONGODB_URL,
   'user model': 'User',
   'cookie secret': process.env.COOKIE_SECRET,
 });
