c7b55298dbce9033039275519374de34ee745b0c
shemogumbe <shem.ogumbe@andela.com>
2019-03-07 20:10:34 +0300

    use dotenv6

--- server/index.js
+++ server/index.js
@@ -1,23 +1,25 @@
-import dotEnv from 'dotenv';
+import config from 'dotenv';
 import keystone from 'keystone';
 import routes from './routes';
 
-dotEnv.config();
+config.config();
 
-// const mongodbURL = () => {
-//   if (process.env.NODE_ENV === 'test') {
-//     const MONGODB_URL = process.env.TEST_MONGODB_URL;
-//     return MONGODB_URL;
-//   }
-//   if (process.env.NODE_ENV === 'production') {
-//     const MONGODB_URL = process.env.MONGODB_URL;
-//     return MONGODB_URL;
-//   }
-//   if (process.env.NODE_ENV === 'development') {
-//     const MONGODB_URL = process.env.DEV_MONGODB_URL;
-//     return MONGODB_URL;
-//   }
-// };
+const mongodbURL = () => {
+  if (process.env.NODE_ENV === 'test') {
+    const MONGODB_URL = process.env.TEST_MONGODB_URL;
+    return MONGODB_URL;
+  }
+  if (process.env.NODE_ENV === 'production') {
+    const MONGODB_URL =
+      process.env.MONGODB_URL || 'mongodb://54.202.70.86:27017/mmdp_db';
+    return MONGODB_URL;
+  }
+  if (process.env.NODE_ENV === 'development') {
+    const MONGODB_URL =
+      process.env.DEV_MONGODB_URL || 'mongodb://54.202.70.86:27017/mmdp_db';
+    return MONGODB_URL;
+  }
+};
 
 keystone.init({
   name: 'MMDP CMS',
@@ -28,7 +30,7 @@ keystone.init({
   'auto update': true,
   session: true,
   auth: true,
-  mongo: 'mongodb://54.202.70.86:27017/mmdp_db',
+  mongo: mongodbURL(),
   'user model': 'User',
   'cookie secret': process.env.COOKIE_SECRET,
 });
