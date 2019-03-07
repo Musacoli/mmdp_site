f98146700c7538e31b369e9727cda6109623bd59
shemogumbe <shem.ogumbe@andela.com>
2019-03-07 16:07:29 +0300

    debu env variables for travis

--- server/index.js
+++ server/index.js
@@ -4,20 +4,20 @@ import routes from './routes';
 
 dotEnv.config();
 
-// const mongodbURL = () => {
-//   if (process.env.NODE_ENV === 'test') {
-//     const MONGODB_URL = process.env.TEST_MONGODB_URL;
-//     return MONGODB_URL;
-//   }
-//   if (process.env.NODE_ENV === 'production') {
-//     const MONGODB_URL = process.env.MONGODB_URL;
-//     return MONGODB_URL;
-//   }
-//   if (process.env.NODE_ENV === 'development') {
-//     const MONGODB_URL = process.env.DEV_MONGODB_URL;
-//     return MONGODB_URL;
-//   }
-// };
+const mongodbURL = () => {
+  if (process.env.NODE_ENV === 'test') {
+    const MONGODB_URL = process.env.TEST_MONGODB_URL;
+    return MONGODB_URL;
+  }
+  if (process.env.NODE_ENV === 'production') {
+    const MONGODB_URL = process.env.MONGODB_URL;
+    return MONGODB_URL;
+  }
+  if (process.env.NODE_ENV === 'development') {
+    const MONGODB_URL = process.env.DEV_MONGODB_URL;
+    return MONGODB_URL;
+  }
+};
 
 keystone.init({
   name: 'MMDP CMS',
@@ -28,7 +28,7 @@ keystone.init({
   'auto update': true,
   session: true,
   auth: true,
-  mongo: 'mongodb://54.202.70.86:27017/mmdp_db',
+  mongo: mongodbURL(),
   'user model': 'User',
   'cookie secret': process.env.COOKIE_SECRET,
 });
